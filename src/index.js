b1a407df132f042b227040eae111f519d8333aaa
thedocbwarren <jedi.master@augmentedjs.com>
2019-06-25 12:01:05 -0700

    add new dialogs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Separated to it's own module, imported for backward compatability
 import { HamburgerMenu, Menu, NavigationMenu, NotificationCenter, Toolbar, Iconbar } from "presentation-navigation";
-import { DialogView, ConfirmationDialogView, AlertDialogView, Notification, Splash } from "presentation-dialogs";
+import { DialogView, ConfirmationDialogView, AlertDialogView, Notification, Splash, ErrorDialogView, MessageDialogView } from "presentation-dialogs";
 
 import Article from "./article/article.js";
 import Card from "./card/card.js";
@@ -12,7 +12,8 @@ module.exports.Article = Article;
 module.exports.Card = Card;
 module.exports.DialogView = DialogView;
 module.exports.ConfirmationDialogView = ConfirmationDialogView;
-module.exports.AlertDialogView = AlertDialogView;
+module.exports.ErrorDialogView = ErrorDialogView;
+module.exports.MessageDialogView = MessageDialogView;
 module.exports.Footer = Footer;
 module.exports.Header = Header;
 module.exports.Manager = Manager;
